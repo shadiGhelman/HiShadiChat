6b44cdcaf2e35fee67503657a8303314f0ce7c90
Shadi Ghelman <shadi.ghelman72@gmail.com>
2020-09-14 11:37:43 +0430

    Initial Commit

---
+++ HiShadiChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HiShadiChat
+//
+//  Created by Shadi Ghelman on 9/14/20.
+//  Copyright © 2020 Shadi Ghelman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
